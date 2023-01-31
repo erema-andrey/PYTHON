07cfbe9f18c5008c0f92c08d66f727354f516b68
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2023-01-31 10:27:24 +0300

    finish002

--- homework.py
+++ homework.py
@@ -58,14 +58,14 @@
 #    print('NO')
 
 
-# n = int(input())
-# m = int(input())
-# k = int(input())
-# area = n * m
-# last = area - k
-# full_m = k / n
-# if full_m == (area - last):
-#     print('YES')
-# else:
-#     print('NO')
+n = int(input())
+m = int(input())
+k = int(input())
+area = n * m
+last = area - k
+full_m = k / n
+if full_m == (area - last):
+    print('YES')
+else:
+    print('NO')
 
