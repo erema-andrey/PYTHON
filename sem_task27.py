adcc57cbd0a0ba3466231c4e7bc70f915e1a4a1b
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2023-02-07 21:17:42 +0300

    saved001

---
+++ sem_task27.py
@@ -0,0 +1,18 @@
+# Задача №27.Пользователь вводит текст(строка). 
+# Словом считается последовательность непробельных символов идущих подряд, 
+# слова разделены одним или большим числом пробелов. Определите, 
+# сколько различных слов содержится в этом тексте.
+
+# Input: She sells sea shells on the sea shore 
+# The shells that she sells are sea shells 
+# I'm sure.So if she sells sea shells on the sea shore 
+# I'm sure that the shells are sea shore shells
+# Output: 13
+
+
+input_str = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+list_str = input_str.lower().split()
+print(list_str)
+res_str = set(list_str)
+print(res_str)
+print(len(res_str))
--- seminar.py
+++ seminar.py
@@ -1,16 +1,53 @@
+# Напишите программу, которая принимает на вход строку, и отслеживает, сколько 
+# раз каждый символ уже встречался.
+# Количество повторов добавляется к символам с помощью постфикса формата _n.
+# Input: a a a b c a a d c d d Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
+# Для решения данной задачи используйте функцию .split()
 
-"""По данному целому неотрицательному n вычислите
-значение n!. N! = 1 * 2 * 3 * … * N (произведение всех
-чисел от 1 до N) 0! = 1 Решить задачу используя цикл
-while
-"""
-user_input = int(input('Введите целое положительное число '))
-result = 1
-n = user_input
-if user_input == 0:
-    print(f'{n}! = 1')
-else:
-    while user_input > 0:
-        result *= user_input
-        user_input -= 1
-    print(f'{n}! ={result}')
+    
+         
+
+# empty = 'a a a b c a a d c d d'
+# list_empty = empty.split()
+# direction = {}
+# res_str = ' '
+# for i in list_empty:
+#     if direction.get(i) != None:
+#         direction[i] += 1
+#         res_str += i+f'_{direction[i]} '
+#     else:
+#         direction[i] = 0
+#         res_str += f'{i} '
+# print(res_str)
+           
+           
+      
+# empty = 'a a a b c a a d c d d'
+# list_empty = empty.split()
+# direction = {}
+# res_str = ' '
+# for i in list_empty:
+#     if direction.get(i) != None:
+#         direction[i] += 1
+#         res_str += i+f'_{direction[i]} '
+#     else:
+#         direction[i] = 0
+#         res_str += f'{i} '
+# print(res_str)
+           
+input_str = 'a a a b c a a d c d d'
+
+input_lst = input_str.split()
+print(input_lst)
+res = []
+
+for i in range(len(input_lst)):
+    x = input_lst[:i]
+    if x.count(input_lst[i]) == 0:
+        res.append(input_lst[i])
+    else:
+        res.append(f'{input_lst[i]}_{x.count(input_lst[i])}')
+print(res)
+out_str = ' '.join(res)
+print(out_str)
+           
