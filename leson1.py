c1a2af8fd9fa3427d9b2df8b7aacf7123d71f789
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2023-02-10 16:12:22 +0300

    saved

--- leson1.py
+++ leson1.py
@@ -23,3 +23,34 @@
    
 # print(quick_sort([1, 2, 3, 6, 9, 2, 23, 16, 24,]))
 
+
+def merge_sort(nums):
+    if len(nums) > 1:
+       mid = len(nums) // 2
+       left = nums[:mid ]
+       right = nums[mid:]
+       merge_sort(left)
+       merge_sort(right)
+       i = j = k = 0
+       while i < len(left) and j < len(right):
+         if left[i] < right[j]:
+            nums[k] = left[i]
+         i += 1
+       else:
+         nums[k] = right[j]
+         j += 1
+         k += 1
+       while i < len(left):
+         nums[k] = left[i]
+         i += 1
+         k += 1
+       while j < len(right):
+         nums[k] = right[j]
+         j += 1
+         k += 1
+list_1 = [38, 27, 43, 3, 9, 82, 10]
+merge_sort(list_1)
+print(list_1)
+      
+      
+      
