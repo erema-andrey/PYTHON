10d9515beb97ba4988e9df4ebcf0c5ab90b0dcec
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2023-02-10 11:40:03 +0300

    saved07

--- leson1.py
+++ leson1.py
@@ -6,7 +6,20 @@
 #n = 700 m = 750 Output: 2
 
 
-n = 700
-m = 750
+# n = 700
+# m = 750
 
-print((m+n-1)//n)
+# print((m+n-1)//n)
+
+# def quick_sort(array):
+#     if len(array) <= 1:
+#          return array
+#     else:
+#          pivot = [0]
+#     less = [i for i in array[1:]if i <= pivot]
+#     greater = [i for i in array[1:]if i > pivot]
+             
+#     return quick_sort(less) + [pivot] + quick_sort(greater)
+   
+# print(quick_sort([1, 2, 3, 6, 9, 2, 23, 16, 24,]))
+
