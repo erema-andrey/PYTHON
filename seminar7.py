a355fc907af08eff427e095f8e21a052323345bf
erema-andrey <115898189+erema-andrey@users.noreply.github.com>
2023-02-17 22:15:02 +0300

    saved09

--- seminar7.py
+++ seminar7.py
@@ -18,5 +18,55 @@
 # # values = transformation = lambda x: x
 
 
+# import math
+# def find_farthest_orbitx(x):
+#     spis = []
+#     for i in x:
+#         if i[0]! =i[1]:
+        
+#         # spis = list(lambda lst: lst[0] * lst[1] * p)
+        
+# from math import pi
+
+
+        
+        
+# from math import pi
+
 # orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
-# print(*find_farthest_orbit(orbits))
+# def find_farthest_orbit(list_of_orbits: list):
+#     for orbit in list_of_orbits:
+#            # если площадь текущей орбиты = максимальной (максимальную находим в списке площадей всех орбит, кроме круговых)
+#         if orbit[0] * orbit[1] * pi == max(map(lambda lst: lst[0] * lst[1] * pi,filter(lambda lst: lst[0] != lst[1], list_of_orbits))):
+#             return orbit
+# print(*find_farthest_orbit(orbits))       
+        
+        
+        
+        
+# Задача №51.Напишите функцию same_by(characteristic, objects), которая проверяет, все ли объекты имеют
+# одинаковое значение некоторой характеристики, и возвращают True, если это так. 
+# Если значение характеристики для разных объектов отличается - то False.
+# Для пустого набора объектов, функция должна возвращать True.
+# Аргумент characteristic - это функция, которая принимает объект и вычисляет его характеристику.
+# Ввод: 
+# Вывод: values = [0, 2, 10, 6] same if same_by(lambda x: x % 2, values):
+# print(‘same’) 
+# else: print(‘different’)
+
+def same_by(characteristic, objects):
+    if len(objects) == 0:
+        return True
+    char_compare = characteristic(objects[0])   # находим значение хар-ки первого эл-та
+    # сравниваем значения хар-к всех эл-тов
+    result_list = list(map(lambda x: characteristic(x) == char_compare, objects))
+    return all(result_list)
+
+values = [0, 2, 10, 6] 
+if same_by(lambda x: x % 2, values):
+    print('same') 
+else: 
+    print('different')
+
+
+
